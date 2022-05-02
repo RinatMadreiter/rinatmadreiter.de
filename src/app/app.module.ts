d29db79f6756da7fb22705accf1290a1020a3d2d
Rinat Madreiter <umfragmail@gmx.de>
2022-05-02 23:24:39 +0200

    added 3 components: 1) Menu; > 2) welcome-section >3) skills-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { WelcomeSectionComponent } from './welcome-section/welcome-section.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    WelcomeSectionComponent,
+    SkillsSectionComponent
   ],
   imports: [
     BrowserModule,
