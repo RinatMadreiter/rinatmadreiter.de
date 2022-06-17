7e8c1bc3d5f0ec5f7e01ad8f8cef8fc83766b877
Rinat Madreiter <umfragmail@gmx.de>
2022-06-17 13:33:25 +0200

    added https source

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -32,4 +32,31 @@ export class MyWorkComponent implements OnInit {
     this.displayAngular = true;
   }
 
+
+  // lazy Loading imgs
+   targets = document.querySelectorAll('img');
+
+   lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+      console.log(entries)
+      entries.forEach(entry => {
+        console.log('😍');
+
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          const src = img.getAttribute('data-lazy');
+
+          img.setAttribute('src', src);
+          img.classList.add('fade');
+
+          observer.disconnect();
+        }
+      });
+    });
+
+    io.observe(target)
+  };
+
+this.targets.forEach(this.lazyLoad);
+
 }
